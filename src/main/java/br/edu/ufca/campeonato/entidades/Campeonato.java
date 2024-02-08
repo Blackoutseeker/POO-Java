826b398990ac226a641f46dfbb6c9151b79d1142
Blackoutseeker <felipeoriginal.2019@gmail.com>
2024-02-08 18:08:18 -0300

    feat(entidades): create jogarPartida method

--- src/main/java/br/edu/ufca/campeonato/entidades/Campeonato.java
+++ src/main/java/br/edu/ufca/campeonato/entidades/Campeonato.java
@@ -2,6 +2,8 @@ package br.edu.ufca.campeonato.entidades;
 
 import java.util.List;
 
+import static br.edu.ufca.campeonato.utils.Gerador.*;
+
 public class Campeonato {
     public List<Clube> clubes;
 
@@ -10,5 +12,27 @@ public class Campeonato {
             throw new IllegalArgumentException("A lista de clubes deve conter no mínimo 2 clubes");
         }
         this.clubes = clubes;
+    }
+
+    /**
+     * Dois placares serão gerados dentro do intervalo [0, 5], "placarM" e "placarV".
+     * @param m Clube que irá receber o placarM, e irá jogar contra o clube V.
+     * @param v Clube que irá receber o placarV, e irá jogar contra o clube M.
+     */
+    private void jogarPartida(Clube m, Clube v) {
+        final int placarM = gerarPlacarAleatorio();
+        final int placarV = gerarPlacarAleatorio();
+        if (placarM > placarV) {
+            m.ganhar(placarM);
+            v.perder(placarV);
+            return;
+        }
+        else if (placarM < placarV) {
+            m.perder(placarM);
+            v.ganhar(placarV);
+            return;
+        }
+        m.empatar();
+        v.empatar();
     }
 }
