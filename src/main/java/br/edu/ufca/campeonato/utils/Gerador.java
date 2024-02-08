f34362a1ea61ff1b676a459e3c45b225cb1d537e
Blackoutseeker <felipeoriginal.2019@gmail.com>
2024-02-08 18:01:52 -0300

    feat(utils): create Gerador class

---
+++ src/main/java/br/edu/ufca/campeonato/utils/Gerador.java
@@ -0,0 +1,8 @@
+package br.edu.ufca.campeonato.utils;
+
+import java.util.Random;
+
+public class Gerador {
+    private static final int QUANTIDADE_DE_NOMES_DE_CLUBES = Constantes.NOMES_DE_CLUBES.length;
+    private static final Random random = new Random();
+}
