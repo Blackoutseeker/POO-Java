6a3988be3da78f82939ce60cf27f0f80049ed0a5
Blackoutseeker <felipeoriginal.2019@gmail.com>
2024-02-08 18:19:55 -0300

    test(GeradorTest): create testGerarListaDeNomesAleatorios method

--- src/test/java/br/edu/ufca/campeonato/GeradorTest.java
+++ src/test/java/br/edu/ufca/campeonato/GeradorTest.java
@@ -4,6 +4,7 @@ import static br.edu.ufca.campeonato.utils.Constantes.NOMES_DE_CLUBES;
 import static br.edu.ufca.campeonato.utils.Gerador.*;
 
 import java.util.Arrays;
+import java.util.List;
 
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
@@ -26,6 +27,25 @@ class GeradorTest {
         for (int indice = 0; indice < 50; indice++) {
             final String nomeAleatorio = gerarNomeAleatorio();
             assertTrue(checarSeNomeDoClubeEstaListado(nomeAleatorio));
+        }
+    }
+
+    @Test
+    void testGerarListaDeNomesAleatorios() {
+        // Deve lançar um erro ao receber uma quantidade de zero nomes
+        assertThrows(IndexOutOfBoundsException.class, () -> gerarListaDeNomesAleatorios(0));
+
+        // Deve lançar um erro ao receber uma quantidade
+        // superior ao número de nomes do array de strings "NOMES_DE_CLUBES"
+        assertThrows(IndexOutOfBoundsException.class, () -> gerarListaDeNomesAleatorios(quantidadeDeNomes + 1));
+
+        // Verifica se a lista gerada possui a mesma quantidade de nomes passados via argumento
+        assertEquals(5, gerarListaDeNomesAleatorios(5).size());
+
+        final List<String> nomesAleatorios = gerarListaDeNomesAleatorios(quantidadeDeNomes);
+        for (final String clubeAleatorio: nomesAleatorios) {
+            // Checa se cada nome aleatório está incluso no array de strings "NOMES_DE_CLUBES"
+            assertTrue(checarSeNomeDoClubeEstaListado(clubeAleatorio));
         }
     }
 }
