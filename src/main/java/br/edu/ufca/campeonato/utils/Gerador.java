1705377428c370a55c7fbc52e3bc9ed41832af12
Blackoutseeker <felipeoriginal.2019@gmail.com>
2024-02-08 18:02:41 -0300

    feat(utils): create gerarNomeAleatorio method

--- src/main/java/br/edu/ufca/campeonato/utils/Gerador.java
+++ src/main/java/br/edu/ufca/campeonato/utils/Gerador.java
@@ -5,4 +5,12 @@ import java.util.Random;
 public class Gerador {
     private static final int QUANTIDADE_DE_NOMES_DE_CLUBES = Constantes.NOMES_DE_CLUBES.length;
     private static final Random random = new Random();
+
+    /**
+     * @return Um nome aleatório do array de strings "NOMES_DE_CLUBES".
+     */
+    public static String gerarNomeAleatorio() {
+        final int indiceAleatorio = random.nextInt(QUANTIDADE_DE_NOMES_DE_CLUBES);
+        return Constantes.NOMES_DE_CLUBES[indiceAleatorio];
+    }
 }
