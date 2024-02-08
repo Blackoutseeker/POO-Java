a05e650fee1196a7320bd553c3a7ad5961248b47
Blackoutseeker <felipeoriginal.2019@gmail.com>
2024-02-08 17:59:27 -0300

    feat(entidades): create ganhar method

--- src/main/java/br/edu/ufca/campeonato/entidades/Clube.java
+++ src/main/java/br/edu/ufca/campeonato/entidades/Clube.java
@@ -13,4 +13,12 @@ public class Clube {
         this.pontos = 0;
         this.saldoGols = 0;
     }
+
+    /** Irá adicionar 3 pontos ao clube, além de somar seu placar ao saldo de gols.
+     * @param saldoGols O número de gols que será somado com o saldo já existente.
+     */
+    public void ganhar(int saldoGols) {
+        pontos += 3;
+        this.saldoGols += saldoGols;
+    }
 }
