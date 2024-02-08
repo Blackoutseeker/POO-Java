43ae11be02399e4e6b6012e060f198e9ed386e88
Blackoutseeker <felipeoriginal.2019@gmail.com>
2024-02-08 18:40:12 -0300

    test(CampeonatoTest): create CampeonatoTest class

---
+++ src/test/java/br/edu/ufca/campeonato/CampeonatoTest.java
@@ -0,0 +1,43 @@
+package br.edu.ufca.campeonato;
+
+import br.edu.ufca.campeonato.entidades.Clube;
+import br.edu.ufca.campeonato.entidades.Campeonato;
+import static br.edu.ufca.campeonato.utils.Constantes.NOMES_DE_CLUBES;
+
+import java.util.List;
+import java.util.ArrayList;
+
+import org.junit.jupiter.api.BeforeAll;
+
+class CampeonatoTest {
+    static private Campeonato campeonato;
+    static private final Clube clubeComMaiorPontuacao = new Clube("Clube A");
+    static private final Clube clubeComMenorPontuacao = new Clube("Clube B");
+
+    @BeforeAll
+    static void setUp() {
+        final List<Clube> clubes = new ArrayList<>();
+        clubeComMaiorPontuacao.pontos = 99;
+        clubeComMaiorPontuacao.saldoGols = 99;
+
+        clubeComMenorPontuacao.pontos = -99;
+        clubeComMenorPontuacao.saldoGols = -99;
+
+        for (int indice = 0; indice < NOMES_DE_CLUBES.length; indice++) {
+            final String nome = NOMES_DE_CLUBES[indice];
+            final Clube clube = new Clube(nome);
+            if (indice == 4) {
+                clubes.add(clubeComMaiorPontuacao);
+                continue;
+            }
+            else if (indice == 10) {
+                clubes.add(clubeComMenorPontuacao);
+                continue;
+            }
+            clubes.add(clube);
+        }
+        campeonato = new Campeonato(clubes);
+        campeonato.jogarCampeonato();
+        campeonato.getClassificacao();
+    }
+}
