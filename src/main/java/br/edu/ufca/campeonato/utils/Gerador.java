0a10ef138bf80e1bbde689b1360db524e3056ee6
Blackoutseeker <felipeoriginal.2019@gmail.com>
2024-02-08 18:04:34 -0300

    feat(utils): create gerarDesempate method

--- src/main/java/br/edu/ufca/campeonato/utils/Gerador.java
+++ src/main/java/br/edu/ufca/campeonato/utils/Gerador.java
@@ -3,6 +3,9 @@ package br.edu.ufca.campeonato.utils;
 import java.util.Random;
 import java.util.List;
 import java.util.ArrayList;
+import java.util.Comparator;
+
+import br.edu.ufca.campeonato.entidades.Clube;
 
 public class Gerador {
     private static final int QUANTIDADE_DE_NOMES_DE_CLUBES = Constantes.NOMES_DE_CLUBES.length;
@@ -46,5 +49,15 @@ public class Gerador {
      */
     public static int gerarPlacarAleatorio() {
         return random.nextInt(6);
+    }
+
+    /** O desempate entre clubes é feito através de suas pontuações, e em casos de pontuações iguais,
+     * seus saldos de gols serão comparados para efetivar o desempate.
+     * @param clubes A lista de clubes que será ordenada conforme as regras de desempate.
+     */
+    public static void gerarDesempate(List<Clube> clubes) {
+         clubes.sort(Comparator.comparingInt((Clube clube) -> clube.pontos)
+                 .thenComparingInt(clube -> clube.saldoGols)
+                 .reversed());
     }
 }
