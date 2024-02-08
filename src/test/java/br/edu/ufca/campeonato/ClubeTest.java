ae19ab6d0b4bba34eab3883a5483068bff35fd80
Blackoutseeker <felipeoriginal.2019@gmail.com>
2024-02-08 18:37:46 -0300

    test(ClubeTest): create testCriacaoDeInstancia method

--- src/test/java/br/edu/ufca/campeonato/ClubeTest.java
+++ src/test/java/br/edu/ufca/campeonato/ClubeTest.java
@@ -3,6 +3,8 @@ package br.edu.ufca.campeonato;
 import br.edu.ufca.campeonato.entidades.Clube;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class ClubeTest {
     private static Clube clube;
@@ -10,5 +12,10 @@ public class ClubeTest {
     @BeforeEach
     void setUp() {
         clube = new Clube("Clube A");
+    }
+
+    @Test
+    void testCriacaoDeInstancia() {
+        assertThrows(IllegalArgumentException.class, () -> new Clube(" "));
     }
 }
