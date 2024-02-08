0969724479f7c077681c9b91ad7de64dfae609cb
Blackoutseeker <felipeoriginal.2019@gmail.com>
2024-02-08 17:50:25 -0300

    feat(utils): create Constantes class

---
+++ src/main/java/br/edu/ufca/campeonato/utils/Constantes.java
@@ -0,0 +1,20 @@
+package br.edu.ufca.campeonato.utils;
+
+public class Constantes {
+    public static final String[] NOMES_DE_CLUBES = {
+            "Athletico Paranaense",
+            "Atlético Mineiro",
+            "Bahia",
+            "Botafogo",
+            "Corinthians",
+            "Cruzeiro",
+            "Flamengo",
+            "Fluminense",
+            "Grêmio",
+            "Internacional",
+            "Palmeiras",
+            "Santos",
+            "São Paulo",
+            "Vasco da Gama",
+    };
+}
